03ab308090d99ba97c687835353d1d234560f6d4
Niverton Felipe <nivertonfelipe355@gmail.com>
2021-05-07 11:38:32 -0300

    Adicionando algoritmo inicial ao DequeEncadeado // TO DO: revisão do codigo

--- DequeEncadeado/DequeEncadeado.java
+++ DequeEncadeado/DequeEncadeado.java
@@ -6,20 +6,56 @@ public class DequeEncadeado {
     private int inseridos;
 
     public DequeEncadeado() {
+        this.cabeca = null;
+        this.rabo = null;
+        this.inseridos = 0;
     }
 
     public void insertFirst(Conta elemento) {
+        //verificar se está vazia (cabeça = elemento, rabo = elemento)
+        //else (temp = cabeça, cabeça = elemento, cabeça.proximo = temp)
+        if(isEmpty()){
+            cabeca = elemento;
+            rabo = elemento;
+        }else {
+            Conta temp = cabeca;
+            cabeca = elemento;
+            cabeca.proximo = temp;
+        }
+        inseridos++;
     }
 
     public void insertLast(Conta elemento) {
+        //verificar se está vazia (cabeça = elemento, rabo = elemento)
+        //else (temp = cabeça, cabeça = elemento, cabeça.proximo = temp)
+        if(isEmpty()){
+            cabeca = elemento;
+            rabo = elemento;
+        }else {
+            rabo.proximo = elemento;
+            rabo = elemento;
+        }
+        inseridos++;
     }
 
     public Conta removeFirst() {
-        return null;
+        Conta elementRemoved = null;
+        if(!isEmpty()){
+            elementRemoved = cabeca;
+            cabeca = cabeca.proximo;
+            inseridos--;
+        }
+        return elementRemoved;
     }
 
     public Conta removeLast() {
-        return null;
+        Conta elementRemoved = null;
+        if(!isEmpty()){
+            elementRemoved = rabo;
+            //anterior a rabo tem que retornar null e virar rabo
+            inseridos--;
+        }
+        return elementRemoved;
     }
 
     public int tamanho() {

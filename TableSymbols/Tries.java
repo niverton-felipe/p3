45107a6160d19b956e9bff09b845ed1af6c060fe
Niverton Felipe <nivertonfelipe355@gmail.com>
2021-08-22 18:53:44 -0300

    Incrementando API com métodos putIterativo, getIterativo e limpa

--- TableSymbols/Tries.java
+++ TableSymbols/Tries.java
@@ -37,10 +37,65 @@ public class Tries<V> {
         return x;
     }
 
+    public void putNaoRecursivo(String key, V val){
+        if (root == null) root = new Node();
+        Node x = root;
+        for(int i = 0; i < key.length(); i++){
+            char c = key.charAt(i);
+            if(x.next[c] == null) x.next[c] = new Node();
+            x = x.next[c];
+        }
+        x.val = val;
+    }
+
+    public V getNaoRecursivo(String key){
+        Node x = root;
+        for(int i = 0; i < key.length(); i++){
+            char c = key.charAt(i);
+            x = x.next[c];
+        }
+        if (x == null) return null;
+        return (V) x.val;
+    }
+
+    public boolean limpa(){
+        if(root == null) return false;
+        boolean result = limpa(root);
+        return result;
+    }
+
+    private boolean limpa(Node x){
+        boolean result = true;
+        boolean folha = true;
+        for(int i = 0; i < R; i++){
+            if(x.next[i] != null){
+                result &= limpa(x.next[i]);
+                folha = false;
+                if (!result) break;
+            }
+        }
+        if(folha && x.val == null) return false;
+        return result;
+    }
+
     public static void main(String[] args) {
         Tries<Integer> tries = new Tries<>();
-        tries.put("by",4);
-        //tries.put("she",0);
+        /*
+        tries.putNaoRecursivo("bye",4);
+        //tries.put("by",4);
+       // System.out.println(tries.getNaoRecursivo("bye"));
+        tries.put("",5);
+        System.out.println(tries.get("by"));
+
+        */
+        //tries.put("a",null);
+        tries.put("avai",2);
+        tries.put("macapa",5);
+        tries.put("zebra",10);
+        tries.put("ola",5);
+        tries.put("flamengo",1);
+
+        System.out.println(tries.limpa());
     }
 }
 

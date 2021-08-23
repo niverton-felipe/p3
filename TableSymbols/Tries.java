2cd30a851a0727037ee5e17287ffbc3f41cfd426
Niverton Felipe <nivertonfelipe355@gmail.com>
2021-08-23 07:27:37 -0300

    Adding delete method to the TriesST API

--- TableSymbols/Tries.java
+++ TableSymbols/Tries.java
@@ -78,6 +78,25 @@ public class Tries<V> {
         return result;
     }
 
+    public void delete(String k){
+        root = delete(root, k, 0);
+    }
+
+    private Node delete(Node x, String k, int d){
+        if (x == null) return null;
+        if(d == k.length()){
+            x.val = null;
+        }else{
+            char c = k.charAt(d);
+            x.next[c] = delete(x.next[c],k,d+1);
+        }
+        if (x.val != null) return x;
+        for(char c = 0; c < R; c++){
+            if (x.next[c] != null) return x;
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         Tries<Integer> tries = new Tries<>();
         /*
